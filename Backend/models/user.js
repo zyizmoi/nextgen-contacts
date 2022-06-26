b065f7f3cb208de46c7c0903d0d485471c15bd14
Zhen Yi <68458835+zyizmoi@users.noreply.github.com>
2022-06-26 19:26:03 +0800

    users db interaction

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
+  contacts: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Contact' }],
 })
 
 userSchema.plugin(uniqueValidator)
