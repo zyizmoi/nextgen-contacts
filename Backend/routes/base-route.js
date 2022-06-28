afc970b782b4a83791da017ae661361b95ccacc1
Zhen Yi <68458835+zyizmoi@users.noreply.github.com>
2022-06-28 10:43:13 +0800

    separate backend

--- Backend/routes/base-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const contactsController = require('../controllers/contacts-controller')
-
-const router = express.Router()
-
-router.get('/', contactsController.allContacts)
-
-module.exports = router
